6c846a54d94b60b1dc7117dd9f3565e485ef90a2
npmajisha <npmajisha@gmail.com>
2015-11-17 12:26:50 -0800

    Indexer with assumed role object

--- indexer.py
+++ indexer.py
@@ -4,11 +4,11 @@ import boto3
 import json
 import argparse
 import logging
-import requests
-from logging import handlers
 import concurrent
+import pysolr
+from logging import handlers
 
-def setup_logging(logfile, loglevel=logging.INFO):
+def setup_logging(logfile, loglevel=logging.DEBUG):
     logger = logging.getLogger('indexer')
     logger.setLevel(loglevel)
     handler = logging.handlers.RotatingFileHandler(
@@ -18,14 +18,17 @@ def setup_logging(logfile, loglevel=logging.INFO):
     logger.addHandler(handler)
     return logger
 
-def index_into_solr(s3client, config, key, logger):
-    if key!=config.get('sourceS3Folder'):
-        solr_url = "http://"+config.get('solrServerPort')+"/solr/"+config.get('solrCoreName')+"/update?commit=true"
+def getS3Object(s3client, s3bucket, key):
+    return json.loads("["+str(s3client.get_object(Bucket=s3bucket, Key=key)['Body'].read())+"]")
+
+def index_into_solr(solr_url, s3client, config, key, logger):
+    solr = pysolr.Solr(solr_url, timeout=10)
+    try:
         payload = json.loads("["+str(s3client.get_object(Bucket=config.get('sourceS3Bucket'), Key=key)['Body'].read(),'utf-8')+"]")
-        headers = {'content-type': 'application/json'}
-        r = requests.get(solr_url, data=json.dumps(payload), headers=headers)
-        if r.status_code != requests.codes.ok:
-            logger.warn("Indexing failed for key %s with content %s" % (key, payload))
+        solr.add(payload,commit=True)
+        logger.info("Indexed %s"%key)
+    except pysolr.SolrError as err:
+        logger.warn("Indexing failed for key %s with error %s" % (key,err))
 
 def main():
     # initialize argument parser
@@ -41,24 +44,31 @@ def main():
     with open(args.config) as file:
         config = json.loads(file.read())
 
-    #initialize s3 resources
-    s3 = boto3.resource('s3')
-    client = boto3.client('s3')
+    if 'assumedRole' in config:
+        sts_client = boto3.client('sts')
+        assumed_role_object = sts_client.assume_role(RoleArn=config.get('assumedRole'),RoleSessionName='solr_indexer')
+        session = boto3.session.Session(assumed_role_object.get('Credentials').get('AccessKeyId'),assumed_role_object.get('Credentials').get('SecretAccessKey'), assumed_role_object.get('Credentials').get('SessionToken'))
+        s3 = session.resource('s3')
+        client = session.client('s3')
+    else:
+        s3 = boto3.resource('s3')
+        client = boto3.client('s3')
 
     bucket = s3.Bucket(config.get('sourceS3Bucket'))
     folder = config.get('sourceS3Folder')
 
+    solr_url = "http://"+ config.get('solrServerPort') + "/solr/" +config.get('solrCoreName')
+
     #spawn threads to index
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-            futures = dict((executor.submit(index_into_solr, client, config, obj.key, logger), obj.key)
-                           for obj in bucket.objects.filter(Prefix=folder))
+            futures = dict((executor.submit(index_into_solr, solr_url, client, config, obj.key, logger), obj.key)
+                           for obj in bucket.objects.filter(Prefix=folder+"/"))
     
             for future in concurrent.futures.as_completed(futures):
                 key = futures[future]
                 if future.exception() is not None:
                     logger.error('%r generated an exception: %s' % (key,
                                                                     future.exception()))
-
     return
 
 
